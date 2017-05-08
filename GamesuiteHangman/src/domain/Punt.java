0d2d995cc4a1c9ff0532348544ae9e9dadc97f6a
Wannes Valvekens <wannes.valvekens@gmail.com>
2017-05-08 15:24:14 +0200

    started Punt Class

---
+++ GamesuiteHangman/src/domain/Punt.java
@@ -0,0 +1,39 @@
+package domain;
+
+public class Punt {
+	
+	private int x;
+	private int y;
+	
+	public Punt(int x, int y){
+		
+		this.setX(x);
+		this.setY(y);
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	private void setX(int x) {
+		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	private void setY(int y) {
+		this.y = y;
+	}
+	
+	public boolean equals(Object object){
+		return false;
+	}
+
+	@Override
+	public String toString() {
+		return "Punt [x=" + x + ", y=" + y + "]";
+	}
+	
+}
