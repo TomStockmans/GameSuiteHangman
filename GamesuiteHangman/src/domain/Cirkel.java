2ab1558a6bc2ca040d144ae584d2860237d265ea
LAPTOP-GBB2T389\Tom Stockmans <tom.stockmans@hotmail.com>
2017-05-09 11:44:50 +0200

    Cirkel klasse is af samen met Testclasse
    denker: Tom Stockmans
    doener: Wannes

--- GamesuiteHangman/src/domain/Cirkel.java
+++ GamesuiteHangman/src/domain/Cirkel.java
@@ -6,22 +6,50 @@ public class Cirkel {
 	Punt middelpunt;
 	
 	public Cirkel(Punt middelpunt, int radius){
+		setMiddelpunt(middelpunt);
+		setRadius(radius);
 		
 	}
 	
 	public int getRadius() {
 		return radius;
 	}
-	public void setRadius(int radius) {
-		//if()
+	
+	private void setRadius(int radius) {
+		if(radius <= 0){throw new DomainException("ongeldige radius!");}
 		this.radius = radius;
 	}
+	
 	public Punt getMiddelpunt() {
 		return middelpunt;
 	}
+	
 	public void setMiddelpunt(Punt middelpunt) {
+		if(middelpunt == null){throw new DomainException("ongeldig middelpunt!");}
 		this.middelpunt = middelpunt;
 	}
+	
+	@Override
+	
+	public boolean equals(Object o){
+		if(o instanceof Cirkel){
+			Cirkel cirkel = (Cirkel)o;
+			
+			if(cirkel.getMiddelpunt() == this.middelpunt && cirkel.getRadius() == this.radius){
+				return true;
+			}
+			
+			
+		}
+		return false;
+	}
+
+	@Override
+	public String toString() {
+		return "Cirkel : middelPunt: " +middelpunt.toString() + " - straal: " + radius ;
+	}
+	
+	
 	
 	
 }
