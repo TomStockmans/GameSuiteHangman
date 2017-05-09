bb505e9f9a7a0939a98b348e9198f01c77d31764
Tom Coppens <tom.coppens@student.ucll.be>
2017-05-09 11:03:12 +0200

    denker: Robbe doener: Tom story 06: eerste test slaagt

--- GamesuiteHangman/src/domain/Driehoek.java
+++ GamesuiteHangman/src/domain/Driehoek.java
@@ -1,24 +1,46 @@
 package domain;
 
 public class Driehoek extends Vorm{
+	
+	private Punt hoekpunt1;
+	private Punt hoekpunt2;
+	private Punt hoekpunt3;
 
-	public Driehoek(Object object, Punt punt2, Punt punt3) {
-		// TODO Auto-generated constructor stub
+	public Driehoek(Punt punt1, Punt punt2, Punt punt3) {
+		this.setHoekPunten(punt1, punt2, punt3);
+	}
+	
+	public void setHoekPunten(Punt punt1, Punt punt2, Punt punt3) throws DomainException{
+		if (punt1==null || punt2==null || punt3==null){
+			throw new DomainException("Hoeken mogen niet null zijn");
+		}
+		this.hoekpunt1=punt1;
+		this.hoekpunt2=punt2;
+		this.hoekpunt3=punt3;
 	}
 
-	public Object getHoekPunt1() {
-		// TODO Auto-generated method stub
-		return null;
+	public Punt getHoekPunt1() {
+		return hoekpunt1;
 	}
 
-	public Object getHoekPunt2() {
-		// TODO Auto-generated method stub
-		return null;
+	public Punt getHoekPunt2() {
+		return hoekpunt2;
 	}
 
-	public Object getHoekPunt3() {
-		// TODO Auto-generated method stub
-		return null;
+	public Punt getHoekPunt3() {
+		return hoekpunt3;
+	}
+	
+	public boolean equals (Object object){
+	if(!(object instanceof Driehoek)) return false;
+	Driehoek driehoek = (Driehoek) object;
+	if(this.getHoekPunt1() == driehoek.getHoekPunt1() && this.getHoekPunt2() == driehoek.getHoekPunt2() && this.getHoekPunt3()==driehoek.getHoekPunt3()) return true;
+	return false;
+	}
+	
+	@Override
+	public String toString() {
+		return "Driehoek met hoekpunten: "+this.getHoekPunt1()+","+this.getHoekPunt2()+","+this.getHoekPunt3();
 	}
 
 }
