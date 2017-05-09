98a89ef9aa1b6298778de3cc38876a1bc7664400
LAPTOP-GBB2T389\Tom Stockmans <tom.stockmans@hotmail.com>
2017-05-09 13:01:06 +0200

    launcher heeft cirkel classe geimplementeerd

--- GamesuiteHangman/src/ui/Launcher.java
+++ GamesuiteHangman/src/ui/Launcher.java
@@ -2,6 +2,8 @@ package ui;
 
 import javax.swing.JOptionPane;
 
+import domain.Cirkel;
+import domain.Punt;
 import domain.Speler;
 
 public class Launcher {
@@ -14,8 +16,15 @@ public class Launcher {
 		
 		String xwaarde = JOptionPane.showInputDialog("Wat is de x-waarde van je punt?");
 		int x = Integer.valueOf(xwaarde);
+		
 		String ywaarde= JOptionPane.showInputDialog("Wat is de y-waarde van je punt?");
 		int y = Integer.valueOf(ywaarde);
+		
+		Punt punt = new Punt(x, y);
+		
+		Cirkel cirkel = new Cirkel(punt, Integer.parseInt(JOptionPane.showInputDialog("Hoe groot moet de straal van de cirkel zijn?")));
+		JOptionPane.showMessageDialog(null, "U hebt een correcte cirkel aangemaakt: " + cirkel.toString());
+		
 	}
 
 }
