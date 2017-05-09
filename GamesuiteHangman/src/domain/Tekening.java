a44d831c9ecf0cab40459f44df233d010e004540
Henok Vermeire <me@henok.xyz>
2017-05-09 21:50:25 +0200

    Added most of Tekening equals is still broken

--- GamesuiteHangman/src/domain/Tekening.java
+++ GamesuiteHangman/src/domain/Tekening.java
@@ -46,6 +46,14 @@ public class Tekening {
     public boolean equals(Object obj) {
         if(!(obj instanceof Tekening)) return false;
         Tekening tekening = (Tekening) obj;
-        return (tekening.vormen.equals(this.vormen) && tekening.getNaam() == this.getNaam());
+        return (arraysEqual(this.vormen, vormen));
+    }
+    private boolean arraysEqual(ArrayList<Vorm> list1, ArrayList<Vorm> list2){
+    	
+        if(list1 == null && list2 == null) return true;
+        if(list1 == null || list2 == null) return false;
+        if(list1.size() == 0 || list2.size() == 0) return true;
+        if(list1.size() != list2.size()) return false;      
+        return (list1.containsAll(list2) /*&& list2.containsAll(list1)*/);
     }
 }
