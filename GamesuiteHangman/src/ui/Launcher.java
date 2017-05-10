bf4c4ea05061ef4eb76e69d169953bd95a7ba2a5
Wannes Valvekens <wannes.valvekens@gmail.com>
2017-05-10 10:17:11 +0200

    Launcher aangepast, lijnstuk aanmaken is toegevoegd

--- GamesuiteHangman/src/ui/Launcher.java
+++ GamesuiteHangman/src/ui/Launcher.java
@@ -2,10 +2,7 @@ package ui;
 
 import javax.swing.JOptionPane;
 
-import domain.Cirkel;
-import domain.DomainException;
-import domain.Punt;
-import domain.Speler;
+import domain.*;
 
 public class Launcher {
 
@@ -17,12 +14,14 @@ public class Launcher {
 		
 		try
 		{
-			String[] keuzes = {"cirkel", "rechthoek"};
+			String[] keuzes = {"cirkel", "rechthoek", "lijnstuk"};
 			String keuze = (String) JOptionPane.showInputDialog(null, "Wat wilt u tekenen:", null, JOptionPane.QUESTION_MESSAGE, null, keuzes, keuzes[0]);
 			switch (keuze) {
 				case "cirkel": maakcirkel();
 					break;
-				case "rechthoek": 
+				case "rechthoek": maakrechthoek();
+					break;
+				case "lijnstuk": maaklijnstuk();
 					break;
 				default:
 					break;
@@ -48,6 +47,7 @@ public class Launcher {
 	}
 	
 	private static void maakcirkel(){
+		JOptionPane.showMessageDialog(null, "U moet een middepunt aanmaken voor de cirkel.");
 		
 		int x = maakwaarde("x coordinaat van het punt");
 		int y = maakwaarde("y coordinaat van het punt");
@@ -59,7 +59,32 @@ public class Launcher {
 	}
 	
 	private static void maakrechthoek(){
+		JOptionPane.showMessageDialog(null, "U moet een linkerbovenhoek aanmaken voor de rechthoek.");
 		
+		int x = maakwaarde("x coordinaat van het punt");
+		int y = maakwaarde("y coordinaat van het punt");
+		Punt punt = new Punt(x, y);
+		JOptionPane.showMessageDialog(null, "U heeft een correct punt aangemaakt: " + punt.toString());
+		
+		Rechthoek rechthoek = new Rechthoek(punt, maakwaarde("breedte van de rechthoek:"), maakwaarde("hoogte van de rechthoek:"));
+		JOptionPane.showMessageDialog(null, "U hebt een correcte rechthoek aangemaakt: " + rechthoek.toString());
+	}
+	
+	private static void maaklijnstuk(){
+		JOptionPane.showMessageDialog(null, "U moet 2 punten aanmaken voor het lijnstuk.");
+		
+		int x1 = maakwaarde("x coordinaat van het eerste punt");
+		int y1 = maakwaarde("y coordinaat van het eerste punt");
+		Punt punt1 = new Punt(x1, y1);
+		JOptionPane.showMessageDialog(null, "U heeft een correct punt aangemaakt: " + punt1.toString());
+		
+		int x2 = maakwaarde("x coordinaat van het tweede punt");
+		int y2 = maakwaarde("y coordinaat van het tweede punt");
+		Punt punt2 = new Punt(x2, y2);
+		JOptionPane.showMessageDialog(null, "U heeft een correct punt aangemaakt: " + punt2.toString());
+		
+		LijnStuk lijnstuk = new LijnStuk(punt1, punt2);
+		JOptionPane.showMessageDialog(null, "U hebt een correct lijnstuk aangemaakt: " + lijnstuk.toString());
 	}
 
 }
